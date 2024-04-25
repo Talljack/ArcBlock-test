03b1273e8242e187df554806bec790b76dda6a2c
Yugang Cao <yugang.cao12@gmail.com>
2024-04-25 09:46:47 +0800

    feat: schema

--- api/src/model/schema.ts
+++ api/src/model/schema.ts
@@ -2,9 +2,9 @@ import { sqliteTable, text } from 'drizzle-orm/sqlite-core'
 
 export const users = sqliteTable('users', {
   id: text('id').primaryKey(),
-  name: text('name'),
-  phone: text('phone'),
-  email: text('email'),
+  name: text('name').notNull(),
+  phone: text('phone').notNull(),
+  email: text('email').unique().notNull(),
   avatar: text('avatar'),
   bio: text('bio'),
 })
